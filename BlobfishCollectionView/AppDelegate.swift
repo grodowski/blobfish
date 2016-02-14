77740c27a3dcb290193bfa520a99698731008ca2
Jan <jgrodowski@gmail.com>
2016-02-14 18:20:56 +0100

    Initial Commit

---
+++ BlobfishCollectionView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BlobfishCollectionView
+//
+//  Created by Jan Grodowski on 14/02/16.
+//  Copyright © 2016 Jan Grodowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BlobfishCollectionView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlobfishCollectionView
+//
+//  Created by Jan Grodowski on 14/02/16.
+//  Copyright © 2016 Jan Grodowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
